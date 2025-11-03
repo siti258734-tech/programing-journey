5746cd52f49cb480db5ffe336bec81a98d16c6d8
siti258734-tech <siti258734@student.smkn1kandeman.sch.id>
2025-11-03 13:36:13 +0700

    Add files via upload

---
+++ Latihan 1 fatimah x pplg1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace latihan_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== TABEL PERKALIAN 5 ===");
+            for (int i = 1; i <= 10; i++)
+            {
+                int hasil = 5 * i;
+                Console.WriteLine("5 * " + i + " = " + hasil);
+            }
+
+        }
+    }
+}
