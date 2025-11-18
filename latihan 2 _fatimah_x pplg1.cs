1f5e090049666a193ab1f0c499c3cf23cfed1e6d
siti258734-tech <siti258734@student.smkn1kandeman.sch.id>
2025-11-18 07:26:03 +0700

    Add files via upload

---
+++ latihan 2 _fatimah_x pplg1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Latihan2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int total = 0;
+
+            while (i <= 5)
+            {
+                total += i;
+                i++;
+            }
+            Console.WriteLine("total = " + total);
+        }
+    }
+}
