be02e5632d90cbd9bced23ecc54ff3f4b2ed947c
siti258734-tech <siti258734@student.smkn1kandeman.sch.id>
2025-11-25 20:26:25 +0700

    Add files via upload

---
+++ PROYEK AKHIR GASAL-KASIR MINI WARUNG PAK JONO-/PRAKTIK 6/contoh array/contoh array/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("contoh array")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("contoh array")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1f80ee8-ab44-4e10-a664-e5371209441f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PROYEK AKHIR GASAL-KASIR MINI WARUNG PAK JONO-/PRAKTIK 6/tugas 6 fatimah x pplg1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace contoh_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat array unyuk menyimpan data siswa
+            string[] nama = new string[3];
+            string[] jeniskelamin = new string[3];
+            string[] kelas = new string[3];
+
+            // input data siswa
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write($"Masukkan nama siswa ke-{i + 1}: ");
+                nama[i] = Console.ReadLine();
+
+                Console.Write("Masukkan jenis kelamin (L/P): ");
+                jeniskelamin [i] = Console.ReadLine();
+
+                Console.Write("Masukkan kelas: ");
+                kelas[i] = Console.ReadLine();
+            }
+            // Menampilkan data siswa 
+            Console.WriteLine("\n=== Data Siswa ===");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Nama: {nama[i]} | JK: {jeniskelamin[i]} | kelas: {kelas[i]} ");
+            }
+            Console.ReadKey(); // Agar jendela konsol tidak langsung menutup
+        }
+    }
+}
